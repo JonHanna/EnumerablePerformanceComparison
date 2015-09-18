3bd74e26dd6441392e60f44020e3de179d629e53
Jon Hanna <jon@hackcraft.net>
2015-09-18 11:20:19 +0100

    Add project

---
+++ src/EnumerablePerformanceComparison/ListCopy.cs
@@ -0,0 +1,350 @@
+﻿namespace System.Collections.Generic
+{
+
+    using System;
+    using System.Runtime;
+    using System.Runtime.Versioning;
+    using System.Diagnostics;
+    using System.Collections.ObjectModel;
+
+    // Cut-down copy the implementation of List<T>, but not be hit by any paths optimised for it.
+    public class ListCopy<T> : IList<T>, IReadOnlyList<T>
+    {
+        private const int _defaultCapacity = 4;
+
+        private T[] _items;
+        private int _size;
+        private int _version;
+        static readonly T[] _emptyArray = new T[0];
+
+        public ListCopy()
+        {
+            _items = _emptyArray;
+        }
+
+        public ListCopy(IEnumerable<T> collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException();
+
+            ICollection<T> c = collection as ICollection<T>;
+            if (c != null)
+            {
+                int count = c.Count;
+                if (count == 0)
+                {
+                    _items = _emptyArray;
+                }
+                else
+                {
+                    _items = new T[count];
+                    c.CopyTo(_items, 0);
+                    _size = count;
+                }
+            }
+            else
+            {
+                _size = 0;
+                _items = _emptyArray;
+
+                using (IEnumerator<T> en = collection.GetEnumerator())
+                {
+                    while (en.MoveNext())
+                    {
+                        Add(en.Current);
+                    }
+                }
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return _items.Length;
+            }
+            set
+            {
+                if (value < _size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                if (value != _items.Length)
+                {
+                    if (value > 0)
+                    {
+                        T[] newItems = new T[value];
+                        if (_size > 0)
+                        {
+                            Array.Copy(_items, 0, newItems, 0, _size);
+                        }
+                        _items = newItems;
+                    }
+                    else
+                    {
+                        _items = _emptyArray;
+                    }
+                }
+            }
+        }
+
+        public int Count
+        {
+            get { return _size; }
+        }
+
+        bool ICollection<T>.IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return _items[index];
+            }
+
+            set
+            {
+                if ((uint)index >= (uint)_size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _items[index] = value;
+                _version++;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (_size == _items.Length) EnsureCapacity(_size + 1);
+            _items[_size++] = item;
+            _version++;
+        }
+
+        public void Clear()
+        {
+            if (_size > 0)
+            {
+                Array.Clear(_items, 0, _size);
+                _size = 0;
+            }
+            _version++;
+        }
+
+        public bool Contains(T item)
+        {
+            if ((Object)item == null)
+            {
+                for (int i = 0; i < _size; i++)
+                    if ((Object)_items[i] == null)
+                        return true;
+                return false;
+            }
+            else
+            {
+                EqualityComparer<T> c = EqualityComparer<T>.Default;
+                for (int i = 0; i < _size; i++)
+                {
+                    if (c.Equals(_items[i], item)) return true;
+                }
+                return false;
+            }
+        }
+
+        public void CopyTo(T[] array)
+        {
+            CopyTo(array, 0);
+        }
+
+        public void CopyTo(int index, T[] array, int arrayIndex, int count)
+        {
+            if (_size - index < count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Array.Copy(_items, index, array, arrayIndex, count);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Array.Copy(_items, 0, array, arrayIndex, _size);
+        }
+
+        private void EnsureCapacity(int min)
+        {
+            if (_items.Length < min)
+            {
+                int newCapacity = _items.Length == 0 ? _defaultCapacity : _items.Length * 2;
+                if ((uint)newCapacity > 0X7FEFFFFF) newCapacity = 0X7FEFFFFF;
+                if (newCapacity < min) newCapacity = min;
+                Capacity = newCapacity;
+            }
+        }
+
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(_items, item, 0, _size);
+        }
+
+        public void Insert(int index, T item)
+        {
+            if ((uint)index > (uint)_size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (_size == _items.Length) EnsureCapacity(_size + 1);
+            if (index < _size)
+            {
+                Array.Copy(_items, index, _items, index + 1, _size - index);
+            }
+            _items[index] = item;
+            _size++;
+            _version++;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if ((uint)index >= (uint)_size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _size--;
+            if (index < _size)
+            {
+                Array.Copy(_items, index + 1, _items, index, _size - index);
+            }
+            _items[_size] = default(T);
+            _version++;
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[_size];
+            Array.Copy(_items, 0, array, 0, _size);
+            return array;
+        }
+
+        public void TrimExcess()
+        {
+            int threshold = (int)(((double)_items.Length) * 0.9);
+            if (_size < threshold)
+            {
+                Capacity = _size;
+            }
+        }
+
+        public struct Enumerator : IEnumerator<T>, System.Collections.IEnumerator
+        {
+            private ListCopy<T> list;
+            private int index;
+            private int version;
+            private T current;
+
+            internal Enumerator(ListCopy<T> list)
+            {
+                this.list = list;
+                index = 0;
+                version = list._version;
+                current = default(T);
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+
+                ListCopy<T> localList = list;
+
+                if (version == localList._version && ((uint)index < (uint)localList._size))
+                {
+                    current = localList._items[index];
+                    index++;
+                    return true;
+                }
+                return MoveNextRare();
+            }
+
+            private bool MoveNextRare()
+            {
+                if (version != list._version)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                index = list._size + 1;
+                current = default(T);
+                return false;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    return current;
+                }
+            }
+
+            Object System.Collections.IEnumerator.Current
+            {
+                get
+                {
+                    if (index == 0 || index == list._size + 1)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    return Current;
+                }
+            }
+
+            void System.Collections.IEnumerator.Reset()
+            {
+                if (version != list._version)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                index = 0;
+                current = default(T);
+            }
+
+        }
+    }
+}
+
---
+++ src/EnumerablePerformanceComparison/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.LinqTest.New;
+
+namespace EnumerablePerformance.Comparison
+{
+    class Program
+    {
+        private const int Iterations = 10000;
+        private static IEnumerable<int> Range(int count)
+        {
+            for (int i = 0; i != count; ++i) yield return i;
+        }
+        public static void Main(string[] args)
+        {
+            foreach (int size in new[] { 10, 16, 100, 128, 1000, 1024 })
+            {
+                MeasurePremutations(Range(size));
+                Console.Error.WriteLine("int");
+                MeasurePremutations(Range(size).Select(i => (long)i));
+                Console.Error.WriteLine("long");
+                MeasurePremutations(Range(size).Select(i => i.ToString()));
+                Console.Error.WriteLine("string");
+                MeasurePremutations(Range(size).Select(i => new DateTime((long)i)));
+                Console.Error.WriteLine("DateTime");
+                MeasurePremutations(Range(size).Select(i => (object)i));
+                Console.Error.WriteLine("boxed int");
+                Console.WriteLine();
+                Console.Error.WriteLine(size);
+            }
+        }
+        private static void MeasurePremutations<T>(IEnumerable<T> basis)
+        {
+            foreach (var seq in Permutations(basis))
+            {
+                bool flip = false;
+                Console.Write(TimeToArray(seq, i => i));
+                Console.Write(',');
+                Console.Write(TimeToArray(seq, i => true));
+                Console.Write(',');
+                Console.Write(TimeToArray(seq, i => true, i => i));
+                Console.Write(',');
+                Console.Write(TimeToArray(seq, i => false));
+                Console.Write(',');
+                Console.Write(TimeToArray(seq, i => false, i => i));
+                Console.Write(',');
+                Console.Write(TimeToArray(seq, i => flip = !flip));
+                Console.Write(',');
+                Console.Write(TimeToArray(seq, i => flip = !flip, i => i));
+                Console.Write(',');
+                Console.Write(TimeToConsume(seq, i => i));
+                Console.Write(',');
+                Console.Write(TimeToConsume(seq, i => true));
+                Console.Write(',');
+                Console.Write(TimeToConsume(seq, i => true, i => i));
+                Console.Write(',');
+                Console.Write(TimeToConsume(seq, i => false));
+                Console.Write(',');
+                Console.Write(TimeToConsume(seq, i => false, i => i));
+                Console.Write(',');
+                Console.Write(TimeToConsume(seq, i => flip = !flip));
+                Console.Write(',');
+                Console.Write(TimeToConsume(seq, i => flip = !flip, i => i));
+                Console.Write(',');
+            }
+        }
+        private static void PreemptGC()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+        private static IEnumerable<IEnumerable<T>> Permutations<T>(IEnumerable<T> basis)
+        {
+            yield return basis;
+            yield return basis.ToArray();
+            yield return basis.ToList();
+            yield return basis.ToList().AsReadOnly();
+            yield return new ListCopy<T>(basis);
+        }
+        private static long TimeToArray<TSource>(IEnumerable<TSource> source, Func<TSource, TSource> selector)
+        {
+            source.Select(selector).ToArray();
+            PreemptGC();
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i != Iterations; ++i)
+                source.Select(selector).ToArray();
+            sw.Stop();
+            return sw.ElapsedTicks;
+        }
+        private static long TimeToArray<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            source.Where(predicate).ToArray();
+            PreemptGC();
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i != Iterations; ++i)
+                source.Where(predicate).ToArray();
+            sw.Stop();
+            return sw.ElapsedTicks;
+        }
+        private static long TimeToArray<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate, Func<TSource, TSource> selector)
+        {
+            source.Where(predicate).Select(selector).ToArray();
+            PreemptGC();
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i != Iterations; ++i)
+                source.Where(predicate).Select(selector).ToArray();
+            sw.Stop();
+            return sw.ElapsedTicks;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void Consume<T>(IEnumerable<T> sequence)
+        {
+            using (var en = sequence.GetEnumerator())
+                while (en.MoveNext())
+                {
+                }
+        }
+        private static long TimeToConsume<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            Consume(source.Select(selector));
+            PreemptGC();
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i != Iterations; ++i)
+                Consume(source.Select(selector));
+            sw.Stop();
+            return sw.ElapsedTicks;
+        }
+        private static long TimeToConsume<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            Consume(source.Where(predicate));
+            PreemptGC();
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i != Iterations; ++i)
+                Consume(source.Where(predicate));
+            sw.Stop();
+            return sw.ElapsedTicks;
+        }
+        private static long TimeToConsume<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, bool> predicate, Func<TSource, TResult> selector)
+        {
+            Consume(source.Where(predicate).Select(selector));
+            PreemptGC();
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i != Iterations; ++i)
+                Consume(source.Where(predicate).Select(selector));
+            sw.Stop();
+            return sw.ElapsedTicks;
+        }
+    }
+}
